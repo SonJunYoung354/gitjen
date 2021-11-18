9e4982d6546a025a570e16485b809bc13906c69a
erty7640@gmail.com <erty7640@gmail.com>
2021-11-18 16:41:49 +0900

    test1

--- http.sh
+++ http.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-rm -rf /var/www/html/index.html
-yum remove -y httpd
+#! /bin/bash
+#rm -rf /var/www/html/index.html
+#yum remove -y httpd
 yum install -y httpd
 chmod 777 /var/www/html
 echo "Jenkins-webhoot-github" >> /var/www/html/index.html
